913fa66c98a2206cdf9ff80390d521b0e3b582ff
Spa-Pregio <support@spa-pregio.com>
2026-03-03 23:50:22 -0500

    Add files via upload

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -41,11 +41,9 @@ export default function Navigation() {
 
             {/* Logo */}
             <Link to="/" className="flex flex-col items-start">
-              <img
-                src="/images/spa-pregio-logo-fixed.png"
-                alt="Spa-Pregio"
-                className="h-8 lg:h-10 w-auto"
-              />
+              <span className="text-lg lg:text-xl font-semibold tracking-widest text-spa-purple">
+                SPA-PREGIO
+              </span>
               <span className="text-[10px] lg:text-xs text-spa-purple/70 uppercase tracking-[0.15em] mt-0.5">
                 The Celebration Suite Movement
               </span>
