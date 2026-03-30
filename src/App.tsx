8b83f14862e6bf02b753a67b245dce4758fc82e5
Spa-Pregio <support@spa-pregio.com>
2026-03-30 16:53:06 -0400

    Add EventDetail route to App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import { useEffect } from 'react';
 import './App.css';
+
 // Layout
 import Navigation from './components/Navigation';
 import Footer from './components/Footer';
+
 // Pages
 import Home from './pages/Home';
 import CelebrationSuites from './pages/CelebrationSuites';
 import Events from './pages/Events';
+import EventDetail from './pages/EventDetail';
 import ForVendors from './pages/ForVendors';
 import Membership from './pages/Membership';
 import About from './pages/About';
@@ -23,15 +26,18 @@ import AdminAffiliates from './pages/AdminAffiliates';
 import Community from './pages/Community';
 import SpoilList from './pages/SpoilList';
 import WhatAboutDad from './pages/WhatAboutDad';
+
 // Funnel Pages
 import ExpectingMom from './pages/ExpectingMom';
 import Vendors from './pages/Vendors';
 
 function ScrollToTop() {
   const { pathname } = useLocation();
+
   useEffect(() => {
     window.scrollTo({ top: 0, behavior: 'smooth' });
   }, [pathname]);
+
   return null;
 }
 
@@ -46,6 +52,7 @@ function App() {
             <Route path="/" element={<Home />} />
             <Route path="/suites" element={<CelebrationSuites />} />
             <Route path="/events" element={<Events />} />
+            <Route path="/events/:id" element={<EventDetail />} />
             <Route path="/vendors" element={<ForVendors />} />
             <Route path="/find-vendors" element={<FindVendors />} />
             <Route path="/join" element={<Membership />} />
@@ -61,7 +68,6 @@ function App() {
             <Route path="/community" element={<Community />} />
             <Route path="/spoil-list" element={<SpoilList />} />
             <Route path="/what-about-dad" element={<WhatAboutDad />} />
-            {/* Funnel Pages */}
             <Route path="/expecting" element={<ExpectingMom />} />
             <Route path="/join-as-vendor" element={<Vendors />} />
           </Routes>
